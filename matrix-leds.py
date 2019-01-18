2f511d62e7d0484631abcc3dc5e93c5f4a8d6fe9
TheTechromancer <thetechromancer@protonmail.com>
2019-01-17 23:24:39 -0600

    fixed sleep import

--- matrix-leds.py
+++ matrix-leds.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import time               
 from lib import opc                                                                                                                                                               
+from time import sleep
 from random import randint
 
 
